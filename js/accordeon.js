bcbd77f1d1e054ebb222b2a167c2fcec238e1478
sere9ka <63472789+sere9ka@users.noreply.github.com>
2021-09-22 22:18:09 +0300

    add toggle function

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,19 +1,17 @@
 const lists = document.querySelectorAll('.feature-sub')
 const btns = document.querySelectorAll('.feature__link') //множество значений
+
 // цикл по перебору кнопок с классом фьючелинк
-btns.forEach((btnItem, index) => {
-    btnItem.addEventListener('click', () => {
-        btns.forEach((btnItem) => {
-            btnItem.classList.remove('feature__link_active')
+btns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        btns.forEach((btnItem, idx) => {
+            if (btnItem === btn) {
+                btnItem.classList.toggle('feature__link_active')
+                lists[idx].classList.toggle('hidden')
+            } else {
+                btnItem.classList.remove('feature__link_active')
+                lists[idx].classList.add('hidden')
+            }
         })
-
-        btnItem.classList.add('feature__link_active')
-
-        lists.forEach((listItem) => {
-            listItem.classList.add('hidden')
-        })
-
-        lists[index].classList.remove('hidden')
-        console.dir(btnItem);
     })
 })
